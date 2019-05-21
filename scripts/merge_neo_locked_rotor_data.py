c4db40cb52218964c99fbff32d96790c2491dd5f
Kestin Goforth <kgoforth1503@gmail.com>
2019-05-20 21:10:38 -0400

    Update neo data

---
+++ scripts/merge_neo_locked_rotor_data.py
@@ -0,0 +1,23 @@
+from glob import glob
+import codecs
+import json
+
+voltages = {
+	"40": "6v",
+	"50": "8v",
+	"60": "10v",
+	"80": "12v",
+}
+
+files = glob('motor_data/NEO Locked-rotor Testing - Raw Data - *A Limit.csv')
+data_dict = {}
+for file in files:
+	import chardet
+	open_file = open(file, "rb")
+	encoding = chardet.detect(open_file.read())['encoding']
+	voltage = voltages[file.split(" ")[-2].strip("A")]
+	data = [[f.strip() for f in e.split(",")] for e in codecs.open(file, 'r', encoding=encoding).read().strip().split("\n")]
+	data_dict[voltage] = [{ 'x': e[0],  'y': e[-1]}  for e in data[1:]]
+
+json.dump(data_dict, open('motor_data/neo-locked-data.json', 'w+'))
+
